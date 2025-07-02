e2cf3d15701e65e6698d9b6653119bb10396e368
jyj8943 <jyj8943@gmail.com>
2025-07-02 11:26:25 +0900

    4-6 카드 만들기
    판정 시스템

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    private int idx = 0;
+    public int idx = 0;
 
     public GameObject front;
     public GameObject back;
@@ -12,16 +12,6 @@ public class Card : MonoBehaviour
     public Animator anim;
 
     public SpriteRenderer frontImage;
-    
-    void Start()
-    {
-        
-    }
-    
-    void Update()
-    {
-        
-    }
 
     public void Setting(int number)
     {
@@ -34,5 +24,42 @@ public class Card : MonoBehaviour
         anim.SetBool("isOpen", true);
         front.SetActive(true);
         back.SetActive(false);
+        
+        // firstCard가 비었다면
+        if (GameManager.instance.firstCard == null)
+        {
+            // firstCard에 내 정보를 넘겨준다
+            GameManager.instance.firstCard = this;
+        }
+        // firstCard가 비어있지 않다면
+        else
+        {
+            // secondCard에 내 정보를 넘겨준다
+            GameManager.instance.secondCard = this;
+            // Matched함수를 호출해 준다.
+            GameManager.instance.Matched();
+        }
+    }
+
+    public void DestroyCard()
+    {
+        Invoke("DestroyCardInvoke", 1.0f);
+    }
+
+    private void DestroyCardInvoke()
+    {
+        Destroy(gameObject);
+    }
+    
+    public void CloseCard()
+    {
+        Invoke("CloseCardInvoke", 1.0f);
+    }
+
+    private void CloseCardInvoke()
+    {
+        anim.SetBool("isOpen", false);
+        front.SetActive(false);
+        back.SetActive(true);
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,45 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+    
+    public Card firstCard;
+    public Card secondCard;
+    
     public Text timeText;
     
     private float time = 0f;
-    
-    void Start()
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
     }
-    
-    void Update()
+
+    private void Update()
     {
         time += Time.deltaTime;
         timeText.text = time.ToString("N2");
+    }
+
+    public void Matched()
+    {
+        if (firstCard.idx == secondCard.idx)
+        {
+            // 파괴
+            firstCard.DestroyCard();
+            secondCard.DestroyCard();
+        }
+        else
+        {
+            // 닫기
+            firstCard.CloseCard();
+            secondCard.CloseCard();
+        }
+
+        firstCard = null;
+        secondCard = null;
     }
 }
